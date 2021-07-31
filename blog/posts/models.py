38b998a4293d0d392dd4b284b7464a5429461541
gokulakannan19 <66069114+gokulakannan19@users.noreply.github.com>
2021-07-31 17:14:33 +0530

    models added

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50, null=True)
+    body = models.CharField(max_length=10000000, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
