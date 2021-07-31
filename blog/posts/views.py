993bd7086da0ae21ff6eea2d9216ea8ea0d5e985
gokulakannan19 <66069114+gokulakannan19@users.noreply.github.com>
2021-07-31 17:34:57 +0530

    data rendered from database

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def index(request):
-    context = {}
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
     return render(request, 'posts/index.html', context)
