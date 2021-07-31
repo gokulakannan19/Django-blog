721d5701bedc290bf7957c78f17aaab62e53fbc4
gokulakannan19 <66069114+gokulakannan19@users.noreply.github.com>
2021-07-31 20:24:56 +0530

    desc function added for displaying blog

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -3,8 +3,16 @@ from .models import Post
 
 
 def index(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('created_at',)[::-1]
     context = {
         'posts': posts
     }
     return render(request, 'posts/index.html', context)
+
+
+def post(request, pk):
+    post = Post.objects.get(id=pk)
+    context = {
+        'post': post
+    }
+    return render(request, 'posts/post.html', context)
